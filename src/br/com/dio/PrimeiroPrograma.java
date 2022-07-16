2c9d3518e914de45652e1f6c215928fa58f72525
NatiellyCristina <natielly_cristina006@hotmail.com>
2022-07-15 23:55:02 -0300

    Remoção de comentários

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -8,9 +8,5 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
 
         System.out.println(gato);
-
-              /*int a = 5;
-        int b = 3;
-                    System.out.println("Hello Word! " + (a+b));*/
     }
 }
